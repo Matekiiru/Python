0b44d617d0268e1f4601a900bfe33b19ecb685b0
Matekiru <Matheus.liburin@gmail.com>
2025-06-11 16:29:30 -0300

    relembrando

---
+++ operadores.py
@@ -0,0 +1,25 @@
+# x = y = z = 0
+
+# x = int(input('Digite um numero: '))
+# y = int(input('Digite outro numero: '))
+
+# z = x + y
+
+# print("A soma dos dois valores é:", z)
+
+
+a = b = c = False
+
+n1 = n2 = 0
+
+n1 = int(input('Digite um Numero: '))
+n2 = int(input('Digite outro Numero:'))
+
+x = n1 == n2
+print('São iguais? ', x, '\n')
+
+z = n1 > n2
+print(n1,'é maior que',n2, '?', z, '\n')
+
+y = n1 != n2
+print('São diferentes?' + str(y))
---
+++ variaveis.py
@@ -0,0 +1,36 @@
+# Variaveis
+
+media = 0
+n1 = n2 = n3 = 0.0
+nome, idade = "Fabio", 47
+estado = True
+
+# Funçao Type() / identifica o tipo do dado inserido
+
+# print(type(media))
+
+# print(type(nome))
+
+# print(type(n1))
+
+# print(type(estado))
+
+# print(type(1+2j))
+
+# Funçao isisntance() / verifica se é do tipo perguntado
+
+# a = 10
+# b = 'Sol'
+
+# print(isinstance(a, int))
+# print(isinstance(a, (int,float)))
+# print(isinstance(b, str))
+
+a = 40
+c = 3
+r = a * c
+
+print(r)
+
+
+# comandos  alt shitf baixo / ctrl K C = comenta as linhas/ ctrl K U = descomenta / ctrl shit aspas = abre o terminal
